174afab56e77ddf8f1504d4e877429b565defb35
Amogh Madan <amoghmadaan@gmail.com>
2026-06-18 20:12:11 +0530

    feat: Add shell -c optional arg

--- src/auth/core/management/commands/__init__.py
+++ src/auth/core/management/commands/__init__.py
@@ -1,8 +1,7 @@
 from auth.core.management.commands.build import build
-from auth.core.management.commands.loaddata import loaddata
 from auth.core.management.commands.runserver import runserver
 from auth.core.management.commands.shell import shell
 
-commands = [build, loaddata, runserver, shell]
+commands = [build, runserver, shell]
 
 __all__ = ["commands"]
--- src/auth/core/management/commands/shell.py
+++ src/auth/core/management/commands/shell.py
@@ -6,8 +6,16 @@ import click
 
 
 @click.command()
+@click.option(
+    "-c",
+    "--cmd",
+    is_flag=False,
+    default=None,
+    help="program passed in as string (terminates option list)",
+)
 @click.pass_context
-def shell(ctx: click.Context) -> None:
+def shell(ctx: click.Context, command: str | None) -> None:
     """Python shell with application context."""
-    args = (sys.executable, "-i")
-    subprocess.call(args, env=os.environ, shell=False)  # nosec: B603
+    params = ["-c", command] if command else ["-i"]
+    cmd = [sys.executable] + params
+    subprocess.call(cmd, env=os.environ, shell=False)  # nosec: B603
