bc4b9cf559f5cdb834855bc8d3deb47a9534342d
Amogh Madan <amoghmadaan@gmail.com>
2026-07-19 23:37:09 +0530

    fix: SCRAPED value change 2 -> 3

--- src/api/schema/resolvers/enums/status.mjs
+++ src/api/schema/resolvers/enums/status.mjs
@@ -6,7 +6,7 @@ const Status = new GraphQLEnumType({
         STATUS_TODO_UNSPECIFIED: { value: 0 },
         STATUS_IN_PROGRESS: { value: 1 },
         STATUS_DONE: { value: 2 },
-        STATUS_SCRAPED: { value: 2 },
+        STATUS_SCRAPED: { value: 3 },
     }
 });
 
